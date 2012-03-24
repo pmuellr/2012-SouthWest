20d7a64143f812300973d2b71d178c0e3138d1b3
Patrick Mueller <pmuellr@apache.org>
2012-03-24 06:59:14 -0400

    change the text forecasts

--- tools/update-weather.sh
+++ tools/update-weather.sh
@@ -13,7 +13,7 @@ echo "   for Zion National Park"
 curl -o 84767-00.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=37.20857&lon=-112.98213&wfo=SLC&zcode=UTZ019&gset=30&gdiff=10&unit=0&tinfo=MY7&ahour=0&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6" 
 curl -o 84767-48.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=37.20857&lon=-112.98213&wfo=SLC&zcode=UTZ019&gset=30&gdiff=10&unit=0&tinfo=MY7&ahour=48&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6"
 
-curl -s "http://mobile.weather.gov/port_mp_ns.php?select=1&CityName=Springdale&site=SLC&State=UT&warnzone=UTZ019" \
+curl -s "http://forecast.weather.gov/MapClick.php?lat=37.2085734&lon=-112.9821253&site=all&smap=1&searchresult=Zion%20National%20Park%2C%20Hurricane%2C%20UT%2084737%2C%20USA&TextType=1" \
     | sed 's_<html>__' \
     | sed 's_<body>__' \
     | sed 's_</html>__' \
@@ -35,7 +35,7 @@ echo "   for Bryce Canyon"
 curl -o 84764-00.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=37.62153&lon=-112.15494&wfo=SLC&zcode=UTZ518&gset=30&gdiff=10&unit=0&tinfo=MY7&ahour=00&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6" 
 curl -o 84764-48.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=37.62153&lon=-112.15494&wfo=SLC&zcode=UTZ518&gset=30&gdiff=10&unit=0&tinfo=MY7&ahour=48&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6"
 
-curl -s "http://mobile.weather.gov/port_mp_ns.php?select=1&CityName=Cannonville&site=SLC&State=UT&warnzone=UTZ020" \
+curl -s "http://forecast.weather.gov/MapClick.php?CityName=Bryce+Canyon&state=UT&site=SLC&textField1=37.6283&textField2=-112.167&e=0&TextType=1" \
     | sed 's_<html>__' \
     | sed 's_<body>__' \
     | sed 's_</html>__' \
@@ -57,7 +57,7 @@ echo "   for Page"
 curl -o 86040-00.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=36.75980&lon=-111.37989&wfo=FGZ&zcode=AZZ009&gset=18&gdiff=3&unit=0&tinfo=MN7&ahour=00&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6" 
 curl -o 86040-48.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=36.75980&lon=-111.37989&wfo=FGZ&zcode=AZZ009&gset=18&gdiff=3&unit=0&tinfo=MN7&ahour=48&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6"
 
-curl -s "http://mobile.weather.gov/port_mp_ns.php?select=1&CityName=Page&site=FGZ&State=AZ&warnzone=AZZ005" \
+curl -s "http://forecast.weather.gov/MapClick.php?CityName=Page&state=AZ&site=FGZ&textField1=36.9086&textField2=-111.473&e=1&TextType=1" \
     | sed 's_<html>__' \
     | sed 's_<body>__' \
     | sed 's_</html>__' \
@@ -79,7 +79,7 @@ echo "   for Grand Canyon"
 curl -o 86023-00.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=36.00432&lon=-112.09533&wfo=FGZ&zcode=AZZ006&gset=18&gdiff=3&unit=0&tinfo=MN7&ahour=00&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6" 
 curl -o 86023-48.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=36.00432&lon=-112.09533&wfo=FGZ&zcode=AZZ006&gset=18&gdiff=3&unit=0&tinfo=MN7&ahour=48&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6"
 
-curl -s "http://mobile.weather.gov/port_mp_ns.php?select=1&CityName=Grand%20Canyon&site=FGZ&State=AZ&warnzone=AZZ006" \
+curl -s "http://forecast.weather.gov/MapClick.php?CityName=Grand+Canyon+Village&state=AZ&site=FGZ&textField1=36.0464&textField2=-112.153&TextType=1" \
     | sed 's_<html>__' \
     | sed 's_<body>__' \
     | sed 's_</html>__' \
@@ -101,7 +101,7 @@ echo "   for Las Vegas"
 curl -o 89109-00.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=36.12856&lon=-115.17113&wfo=VEF&zcode=NVZ020&gset=18&gdiff=5&unit=0&tinfo=PY8&ahour=00&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6" 
 curl -o 89109-48.png -s "http://forecast.weather.gov/wtf/meteograms/Plotter.php?lat=36.12856&lon=-115.17113&wfo=VEF&zcode=NVZ020&gset=18&gdiff=5&unit=0&tinfo=PY8&ahour=48&pcmd=110111111111100000000000000000000000000000000000000000000&lg=en&indu=1!1!1&dd=0&bw=0&hrspan=48&pqpfhr=6&psnwhr=6"
 
-curl -s "http://mobile.weather.gov/port_mp_ns.php?select=1&CityName=Las%20Vegas&site=VEF&State=NV&warnzone=NVZ020" \
+curl -s "http://forecast.weather.gov/MapClick.php?CityName=Las+Vegas&state=NV&site=VEF&textField1=36.175&textField2=-115.136&e=0&TextType=1" \
     | sed 's_<html>__' \
     | sed 's_<body>__' \
     | sed 's_</html>__' \
